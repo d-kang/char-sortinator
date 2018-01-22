0cb244d6ba7f7f8dfcbdec240b2a280cfe75b235
d-kang <david.kang714@gmail.com>
2018-01-22 14:11:52 -0800

    Add Express Middleware

--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
+const logger = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const data = [];
 
 app.get('/', (req, res) => res.send('hello wor;d'));
-
-
 app.listen(3000, () => console.log('app is listening on port http://localhost:3000'));
