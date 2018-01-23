c66155875e7d582962c66931d76e3afe6be9a70f
d-kang <david.kang714@gmail.com>
2018-01-22 17:49:31 -0800

    Added Jest Tests to Server Using Promises and Eslint Config

--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
-To Start enter `npm start`
+To start server enter `npm start`
+To start dev server enter `npm run devserver`
--- src/sum.test.js
+++ src/sum.test.js
@@ -1,5 +1,46 @@
-const sum = (a, b) => a + b;
+const request = require('request');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('posts form data server and response is sorted', () => {
+  const apiEndpoint = 'http://localhost:3001/api/sort';
+  const formData = { form: {payload: 'hello'}}
+  const myPromise = () => {
+    return new Promise((resolve, reject) => {
+      request.post(apiEndpoint, formData, (e, res, body) => {
+        return e ? reject(e) : resolve(body);
+      });
+    });
+  }
+
+  myPromise()
+    .then(res => (console.log('res', res), JSON.parse(res)))
+    .then(res => expect(res).toEqual(["hello","ehllo"]))
+    .catch(console.error);
+});
+
+
+test('gets an array of tuples from server and ensures each input and output are correct', () => {
+  const apiEndpoint = 'http://localhost:3001/api/sort';
+  const myPromise = () => {
+    return new Promise((resolve, reject) => {
+      request.get(apiEndpoint, (e, res, body) => {
+        return e ? reject(e) : resolve(body);
+      });
+    });
+  }
+
+  myPromise()
+    .then(res => (console.log('res2', res), JSON.parse(res)))
+    .then(res => {
+      if (res.length > 0) {
+        for (var i = 0; i < res.length; i++) {
+          const input = res[i][0];
+          const output = res[i][1];
+          const transformFirst = input.split('').sort().join('');
+          console.log('transformFirst', transformFirst);
+          console.log('output', output);
+            expect(transformFirst).toEqual(output);
+        }
+      }
+    })
+    .catch(console.error);
 })
